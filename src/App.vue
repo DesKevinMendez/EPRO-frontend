7f1b183ff92a9c50458780c2074756df6e22a3d2
Kevin Mendez <kevin@servo-app.com>
2019-03-02 09:59:44 -0600

    Diseño de Home

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,26 @@
   <v-app>
     <Nav/>
     <v-content>
+      <v-container fluid >
       <router-view/>
+      </v-container>
     </v-content>
+    <Footer/>
   </v-app>
 </template>
 
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import Nav from '@/components/Nav.vue';
+import Footer from "@/components/partials/Footer.vue";
+import Nav from "@/components/Nav.vue";
 @Component({
   name: "App",
-  components: { Nav }
+  components: { Nav, Footer }
 })
 export default class App extends Vue {
   data() {
     return {};
   }
-};
+}
 </script>
---
+++ src/components/partials/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-footer dark height="auto" class="m-5">
+    <v-card class="flex" flat tile>
+      <v-card-title class="blue">
+        <strong class="subheading">Get connected with us on social networks!</strong>
+        <v-spacer></v-spacer>
+      </v-card-title>
+
+      <v-card-actions class="grey darken-3 justify-center">
+        &copy;2019 —
+        <strong>Estándares de programación</strong>
+      </v-card-actions>
+    </v-card>
+  </v-footer>
+</template>
+<script>
+export default {
+  name: "footer"
+};
+</script>
