5d97ff459533bd0739640e78726ee98ef6be548b
Kevin Mendez <yokevinmendez@gmail.com>
2019-05-20 09:49:04 -0600

    Muestra la vista de visitantes, y agrega nuevo visitantes, asi como genera reportes de los visitantes

--- src/utils/HTTP.ts
+++ src/utils/HTTP.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// const URL: string = "http://127.0.0.1:8000/api/";
+const URL: string = "http://127.0.0.1:8000/api/";
 // const URL: string = "http://192.168.208.131/api/";
-const URL: string = "https://estandares-programacion.herokuapp.com/api/";
+// const URL: string = "https://estandares-programacion.herokuapp.com/api/";
 
 const http = axios.create({
   baseURL: URL
---
+++ src/views/Visitas.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-layout row wrap class="mt-4">
+    <v-flex xs9>
+      <Visita :visitas="infoInvitados"/>
+    </v-flex>
+    <v-flex xs3>
+      <NuevoInvitado @datosInvitados="nuevoDatos"/>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { http } from "@/utils/HTTP.ts";
+import Visita from "@/components/Visitas/visita.vue";
+import NuevoInvitado from "@/components/Visitas/InvitarNuevo.vue";
+
+@Component({
+  name: "VisitasRec",
+  components: { Visita, NuevoInvitado },
+  methods: {
+    nuevoDatos(event) {
+      this.getVisitas();
+    }
+  }
+})
+class VistasRecibidas extends Vue {
+  data() {
+    return {
+      datos: [],
+      infoInvitados: []
+    };
+  }
+
+  mounted() {
+    this.getVisitas();
+  }
+
+  getVisitas() {
+    http.get("invitados").then((res) => {
+      this.$data.infoInvitados = res.data;
+    });
+  }
+
+}
+export default VistasRecibidas;
+</script>
